b7bb06e36f2d6510ce8887077a101ddf425ef406
M1ngXU <91617361+M1ngXU@users.noreply.github.com>
2022-01-02 14:42:59 +0000

    added interpreter partly

--- src/state.rs
+++ src/state.rs
@@ -3,7 +3,7 @@ use std::fmt::{ Display, Debug };
 use std::fmt;
 use std::ops::{ Index, IndexMut };
 
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Copy, Clone)]
 struct Byte {
 	data: u8
 }
@@ -60,7 +60,7 @@ impl Byte {
 }
 
 
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Copy, Clone)]
 struct TwoBytes {
 	data: u16
 }
@@ -91,6 +91,10 @@ impl TwoBytes {
 		self.data as usize
 	}
 
+	pub fn as_byte(&self) -> Byte {
+		Byte::from(self.data as u8)
+	}
+
 	pub fn mask_bits(&self, mask: &Self) -> Self {
 		Self::from(self.data & mask.data)
 	}
@@ -114,6 +118,14 @@ impl TwoBytes {
 	pub fn get_bit(&self, bit: u8) -> bool {
 		self.mask_bits(&TwoBytes::from(1).shift_left(bit)).shift_right(bit).data == 1
 	}
+
+	pub fn add(&mut self, amount: u16) {
+		self.data += amount;
+	}
+
+	pub fn add_byte(&mut self, byte: Byte) {
+		self.data += byte.data;
+	}
 }
 
 
@@ -130,6 +142,10 @@ impl Memory {
 			new.data.push(Byte::new());
 		}
 		new
+	}
+
+	pub fn get_two_bytes(&self, start: TwoBytes) -> TwoBytes {
+		TwoBytes::from((self[start].data as u16) << 8 + self[start.data as usize + 1].data)
 	}
 }
 impl Index<TwoBytes> for Memory {
@@ -171,6 +187,34 @@ impl DataRegisters {
 		}
 		new
 	}
+
+	pub fn get_f(&self) -> Byte {
+		self.data[0xF]
+	}
+	
+	pub fn set_f(&mut self, v: Byte) {
+		self.data[0xF] = v;
+	}
+
+	pub fn get_0(&self) -> Byte {
+		self.data[0x0]
+	}
+
+	pub fn get_x(&mut self, v: TwoBytes) -> Byte {
+		self.data[v.shift_right(8).mask_bits(&TwoBytes::from(0xF)).as_usize()]
+	}
+
+	pub fn set_x(&mut self, v: TwoBytes, value: Byte) {
+		self.data[v.shift_right(8).mask_bits(&TwoBytes::from(0xF)).as_usize()] = value;
+	}
+
+	pub fn get_y(&mut self, v: TwoBytes) -> Byte {
+		self.data[v.shift_right(4).mask_bits(&TwoBytes::from(0xF)).as_usize()]
+	}
+
+	pub fn set_y(&mut self, v: TwoBytes, value: Byte) {
+		self.data[v.shift_right(4).mask_bits(&TwoBytes::from(0xF)).as_usize()] = value;
+	}
 }
 impl Index<Byte> for DataRegisters {
 	type Output = Byte;
@@ -210,23 +254,13 @@ impl AdressRegister {
 }
 
 
-struct Stack {
-	data: LinkedList<TwoBytes>
-}
-impl Stack {
-	pub fn new() -> Self {
-		Self {
-			data: LinkedList::new()
-		}
-	}
-}
-
-
 struct State {
 	memory: Memory,
 	data_registers: DataRegisters,
 	adress_register: AdressRegister,
-	stack: Stack
+	stack: LinkedList<TwoBytes>,
+	pc: TwoBytes,
+	screen: super::screen::Screen
 }
 impl State {
 	pub fn new_chip8() -> Self {
@@ -234,7 +268,9 @@ impl State {
 			memory: Memory::new(4096),
 			data_registers: DataRegisters::new(16),
 			adress_register: AdressRegister::new(),
-			stack: Stack::new()
+			stack: LinkedList::new(),
+			pc: TwoBytes::from(0x200),
+			screen: super::screen::Screen::new(64, 32)
 		};
 		for (i, b) in [
 			0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
@@ -257,6 +293,54 @@ impl State {
 			new.memory[i] = Byte::from(b);
 		}
 		new
+	}
+
+	pub fn interpret_next(&mut self) -> bool {
+		let current = self.memory.get_two_bytes(self.pc);
+		let x = self.data_registers.get_x(current);
+		let y = self.data_registers.get_y(current);
+		let l3_const = current.mask_bits(&TwoBytes::from(0x0FFF));
+		let l2_const = current.mask_bits(&TwoBytes::from(0x00FF)).as_byte();
+		let l1_const = current.mask_bits(&TwoBytes::from(0x000F)).as_byte();
+		match current.shift_right(12).as_usize() {
+			0x0 | 0x1 | 0x2 | 0xB => {
+				match current.shift_right(12).as_usize() {
+					0x0 => match l3_const.data {
+							0x00E0 => {
+								self.screen.clear();
+								self.pc.add(2);
+							}, 0x00EE => {
+								self.pc = self.stack.pop_back().unwrap();
+							}, _ => {
+								todo!("not necessary?");
+							}
+					}, 0x1 => {
+						if self.pc == l3_const {
+							return true;
+						}
+						self.pc = l3_const;
+					}, 0x2 => {
+						self.stack.push_back(self.pc);
+						self.pc = l3_const;
+					}, 0xB => {
+						self.pc = l3_const;
+						self.pc.add_byte(self.data_registers.get_0());
+					}, _ => unimplemented!()
+				}
+			}, _ => {
+				self.pc.add(2);
+				match current.shift_right(12).as_usize() {
+					0x3000 => if x == l2_const {
+						self.pc.add(2)
+					}, 0x4000 => if x != l2_const {
+						self.pc.add(2);
+					}, 0x5000 => if x == y {
+						self.pc.add(2);
+					}
+				}
+			}
+		}
+		false
 	}
 }
 
