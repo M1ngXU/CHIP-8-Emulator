61470999faf6e410a07eddaf1bde1cf8cd19ad77
M1ngXU <>
2022-01-03 13:16:39 +0000

    removed depency stuff

--- target/debug/deps/my_project-d99c75aada7cfe1a.d
+++
@@ -1,7 +0,0 @@
-/home/runner/CHIP-8/target/debug/deps/my_project-d99c75aada7cfe1a.rmeta: src/main.rs src/state.rs src/screen.rs
-
-/home/runner/CHIP-8/target/debug/deps/my_project-d99c75aada7cfe1a.d: src/main.rs src/state.rs src/screen.rs
-
-src/main.rs:
-src/state.rs:
-src/screen.rs:
